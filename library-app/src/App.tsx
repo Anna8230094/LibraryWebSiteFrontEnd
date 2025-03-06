63039d5605d8da31de33c9c29d226f3b0d3247c1
Anna Megalou <annamegalou3@gmail.com>
2025-03-06 12:48:23 +0200

    ftr:creating first component

--- library-app/src/App.tsx
+++ library-app/src/App.tsx
@@ -1,25 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <nav className="navbar navbar-expand-lg navbar-dark main-color py-3">
+      <div className="container-fluid">
+        <span className="navbar-brand">Luv 2 Read</span>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-tartget="#navbarNavDropDown"
+          aria-controls="navbarNavDropDown"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collaps navbar-collaps" id="navbarDropDown">
+          <ul className="navbar-nav">
+            <li className="navbar-item">
+              <a className="nav-link" href="#">
+                Home
+              </a>
+            </li>
+            <li className="navbar-item">
+              <a className="nav-link" href="#">
+                Search Books
+              </a>
+            </li>
+          </ul>
+          <ul className="navbar-nav ms-auto">
+            <li className="navbar-item m-1">
+              <a type="button" className="nav-link" href="#">
+                Home
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 }
 
