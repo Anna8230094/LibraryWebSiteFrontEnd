18afbe36f286ac85a285b1fa44c8bd1b80fc4f39
Anna Megalou <annamegalou3@gmail.com>
2025-03-07 22:00:52 +0200

    starting hero component

---
+++ library-app/src/layouts/HomePage/Heros.tsx
@@ -0,0 +1,18 @@
+export const Heros = () => {
+  return (
+    <div>
+      <div className="d-none d-lg-block">
+        <div className="row g-0 mt-5">
+            <div className="col-image-left ">
+
+            </div>
+            <div className="col-4 col-md-4 container d-flex justify-content-center align-items-center">
+                <div className="ml-2">
+                    <h1>w</h1>
+                </div>
+            </div>
+        </div>
+      </div>
+    </div>
+  );
+};
