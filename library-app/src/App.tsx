83270906e4057931c58f221aa944687a094f88e4
Anna Megalou <annamegalou3@gmail.com>
2025-03-07 21:02:11 +0200

    ftr:Creating Carousel component

--- library-app/src/App.tsx
+++ library-app/src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Nabar from "./layouts/NavbarAndFooter/Navbar";
 import ExploreTopBooks from "./layouts/HomePage/ExploreTopBooks";
+import Carousel from "./layouts/HomePage/Carousel";
 
 function App() {
   return (
     <div>
       <Nabar />
       <ExploreTopBooks />
+      <Carousel/>
     </div>
   );
 }
