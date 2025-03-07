735f4b71ef3ae3cd37463a3a974c1372f8113922
Anna Megalou <annamegalou3@gmail.com>
2025-03-07 18:43:48 +0200

    ftr:Creating ExplorTopBook and NavBar

--- library-app/src/App.tsx
+++ library-app/src/App.tsx
@@ -1,45 +1,14 @@
 import React from "react";
 import "./App.css";
+import Nabar from "./layouts/NavbarAndFooter/Navbar";
+import ExploreTopBooks from "./layouts/HomePage/ExploreTopBooks";
 
 function App() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark main-color py-3">
-      <div className="container-fluid">
-        <span className="navbar-brand">Luv 2 Read</span>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-tartget="#navbarNavDropDown"
-          aria-controls="navbarNavDropDown"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collaps navbar-collaps" id="navbarDropDown">
-          <ul className="navbar-nav">
-            <li className="navbar-item">
-              <a className="nav-link" href="#">
-                Home
-              </a>
-            </li>
-            <li className="navbar-item">
-              <a className="nav-link" href="#">
-                Search Books
-              </a>
-            </li>
-          </ul>
-          <ul className="navbar-nav ms-auto">
-            <li className="navbar-item m-1">
-              <a type="button" className="nav-link" href="#">
-                Home
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+    <div>
+      <Nabar />
+      <ExploreTopBooks />
+    </div>
   );
 }
 
