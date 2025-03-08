33bffac15532bdcc3674ab6f9b5ac8853b1d87d5
Anna Megalou <annamegalou3@gmail.com>
2025-03-08 13:39:27 +0200

    trying tio fix hero section

--- library-app/src/App.tsx
+++ library-app/src/App.tsx
@@ -3,13 +3,15 @@ import "./App.css";
 import Nabar from "./layouts/NavbarAndFooter/Navbar";
 import ExploreTopBooks from "./layouts/HomePage/ExploreTopBooks";
 import Carousel from "./layouts/HomePage/Carousel";
+import { Heros } from "./layouts/HomePage/Heros";
 
 function App() {
   return (
     <div>
       <Nabar />
       <ExploreTopBooks />
-      <Carousel/>
+      <Carousel />
+      <Heros />
     </div>
   );
 }
