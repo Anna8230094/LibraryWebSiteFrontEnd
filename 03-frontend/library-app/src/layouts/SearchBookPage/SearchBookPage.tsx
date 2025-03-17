8f22aa44bc7e7e8a143a08cef3013bef36b20d45
Anna Megalou <annamegalou3@gmail.com>
2025-03-17 18:49:03 +0200

    ftr:Adding Search functionallity

--- 03-frontend/library-app/src/layouts/SearchBookPage/SearchBookPage.tsx
+++ 03-frontend/library-app/src/layouts/SearchBookPage/SearchBookPage.tsx
@@ -1,9 +1,8 @@
-import { useEffect, useState } from "react";
+import { use, useEffect, useState } from "react";
 import BookModel from "../../Models/BookModel";
 import { SpinnerLoading } from "../Utils/SpinnerLoading";
 import { SearchBook } from "./components/SearchBook";
 import { Pagination } from "../Utils/Pagination";
-
 
 export const SearchBookPage = () => {
   const [books, setBooks] = useState<BookModel[]>([]);
@@ -13,12 +12,21 @@ export const SearchBookPage = () => {
   const [booksPerPage, setBooksPerPage] = useState(5);
   const [totalAmountOfBooks, setTotalAmmountOfBook] = useState(0);
   const [totalPages, setTotalPages] = useState(0);
+  const [search, setSearch] = useState("");
+  const [searchUrl, setSearchUrl] = useState("");
 
   useEffect(() => {
     const fetchBooks = async () => {
       //await promise to come back
       const baseUrl: string = "http://localhost:8080/api/books";
-      const url: string = `${baseUrl}?page=${currentPage-1}&size=${booksPerPage}`;
+      let url: string = "";
+
+      if (searchUrl === "") {
+        url = `${baseUrl}?page=${currentPage - 1}&size=${booksPerPage}`;
+      } else {
+        url = baseUrl + searchUrl;
+      }
+
       const response = await fetch(url);
       if (!response.ok) {
         throw new Error("Something went wrong");
@@ -27,7 +35,7 @@ export const SearchBookPage = () => {
       const responseData = resposeJson._embedded.books; // we want to call _embeded.books that we dont have to reuse this every single time
 
       setTotalAmmountOfBook(resposeJson.page.totalElements);
-      setTotalPages(resposeJson.page.totalPages)
+      setTotalPages(resposeJson.page.totalPages);
       const loadedBooks: BookModel[] = [];
 
       for (const key in responseData) {
@@ -49,8 +57,8 @@ export const SearchBookPage = () => {
       setLoading(false);
       setHttpError(error.message);
     });
-    window.scrollTo(0,0);//Every time that we change page this command will return us in the top of our web site
-  }, [currentPage]);//we want tto recall this current hook
+    window.scrollTo(0, 0); //Every time that we change page this command will return us in the top of our web site
+  }, [currentPage, searchUrl]); //we want tto recall this current hook
 
   if (isLoading) {
     return <SpinnerLoading />;
@@ -64,10 +72,21 @@ export const SearchBookPage = () => {
     );
   }
 
-const indexOfLastBook: number = currentPage * booksPerPage;
-const indexOfFirstBook:number = indexOfLastBook - booksPerPage;
-let lastItem = booksPerPage * currentPage <= totalAmountOfBooks ? booksPerPage * currentPage  : totalAmountOfBooks;
-const paginate = (pageNumber : number) => setCurrentPage(pageNumber);
+  const searchHandleChange = () => {
+    if (search === '') {
+      setSearchUrl('');
+    }else{
+      setSearchUrl(`/search/findByTitleContaining?title=${search}&page=0&size=${booksPerPage}`)
+    }
+  }
+
+  const indexOfLastBook: number = currentPage * booksPerPage;
+  const indexOfFirstBook: number = indexOfLastBook - booksPerPage;
+  let lastItem =
+    booksPerPage * currentPage <= totalAmountOfBooks
+      ? booksPerPage * currentPage
+      : totalAmountOfBooks;
+  const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
 
   return (
     <div className="container">
@@ -80,8 +99,11 @@ const paginate = (pageNumber : number) => setCurrentPage(pageNumber);
                 type="search"
                 placeholder="Seaech"
                 aria-labelledby="Search"
+                onChange={e =>setSearch(e.target.value)}
               />
-              <button className="btn btn-btn-outline-success">Search</button>
+              <button className="btn btn-btn-outline-success" onClick={() => searchHandleChange()}>
+                Search
+             </button>
             </div>
           </div>
           <div className="col-4">
@@ -122,11 +144,19 @@ const paginate = (pageNumber : number) => setCurrentPage(pageNumber);
           <div className="mt-3">
             <h5>Number of results:({totalAmountOfBooks})</h5>
           </div>
-          <p>{indexOfFirstBook+1} to lastItem of {totalAmountOfBooks} items</p>
+          <p>
+            {indexOfFirstBook + 1} to lastItem of {totalAmountOfBooks} items
+          </p>
           {books.map((book) => (
             <SearchBook book={book} key={book.id} />
           ))}
-          {totalPages >1 && <Pagination currentPage={currentPage} totalPages={totalPages} paginate={paginate}/>}
+          {totalPages > 1 && (
+            <Pagination
+              currentPage={currentPage}
+              totalPages={totalPages}
+              paginate={paginate}
+            />
+          )}
           {/*only render pagination if totalPages are >1 */}
         </div>
       </div>
